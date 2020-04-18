7ee65165538bd5dab5ff7308e277129a61aac711
vilboot <vilboot@hotmail.com>
2020-04-18 15:44:08 +0200

    add resources and round meca

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,51 @@ public class GameManager : MonoBehaviour {
         return INSTANCE;
     }
 
+    public enum RoudState {
+        PREPARATION, ATTACK
+    }
+
+    private Resources resourcesA = new Resources(0, Resources.ResourcesType.A);
+    private Resources resourcesB = new Resources(0, Resources.ResourcesType.B);
+    private Resources resourcesC = new Resources(0, Resources.ResourcesType.C);
+    private Resources resourcesD = new Resources(0, Resources.ResourcesType.D);
+    
+    public int roundCount;
+    public RoudState roudState;
+
     void Awake() {
         if (INSTANCE == null) {
             INSTANCE = this;
         }
+    }
+
+    void Start() {
+        roundCount = 0;
+        roudState = RoudState.PREPARATION;
+    }
+
+    public void SwitchState() {
+    	if (roudState == RoudState.PREPARATION) {
+    		roudState = RoudState.ATTACK;
+    		return;
+    	}
+    	roudState = RoudState.PREPARATION;
+    	roundCount += 1;
+    }
+
+    public Resources GetResourcesA() {
+    	return resourcesA;
+    }
+
+    public Resources GetResourcesB() {
+    	return resourcesB;
+    }
+
+    public Resources GetResourcesC() {
+    	return resourcesC;
+    }
+
+    public Resources GetResourcesD() {
+    	return resourcesD;
     }
 }
---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Resources {
+
+
+    public enum ResourcesType {
+        A, B, C, D
+    }
+
+    public ResourcesType type = ResourcesType.A;
+    public int count = 0;
+
+    public Resources(int countValue, ResourcesType typeValue) {
+        count = countValue;
+        type = typeValue;
+    }
+
+    public void Add(int value) {
+        count += value;
+    }
+
+    public bool Use(int value) {
+        if (value > count) {
+            return false;
+        }
+        count -= value;
+        return true;
+    }
+}
