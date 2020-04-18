753e7ecb5aa49172fde457b19a36dc0a747be9d3
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-04-18 21:02:34 +0200

    Scene Leo

---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineDrawer : MonoBehaviour
+{
+
+    private LineRenderer lineRend;
+    private Transform otherBuilding;
+
+
+
+    void Start()
+    {
+
+        lineRend = GetComponent<LineRenderer>();
+        otherBuilding = GameObject.FindGameObjectWithTag("Building").transform;
+
+    }
+
+
+    void Update()
+    {
+
+        lineRend.SetPosition(0, transform.position);
+        lineRend.SetPosition(1, otherBuilding.transform.position);
+
+    }
+}
---
+++ Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+
+    public float speedRotationMin;
+    public float speedRotationDelta;
+    private float speedRotation;
+
+    public bool clockwise;
+
+    void Awake()
+    {
+        speedRotation = Random.Range(0f, speedRotationDelta) + speedRotationMin;
+        clockwise = (Random.Range(0, 2) == 0);
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, speedRotation * Time.deltaTime * (clockwise ? 1 : -1));
+    }
+}
