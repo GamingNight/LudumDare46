fb09e77fae7b064ddb3b3efc1e0c43f967984a4b
vilboot <vilboot@hotmail.com>
2020-04-19 15:58:20 +0200

    add score

---
+++ Assets/Scripts/TextFooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFooter : MonoBehaviour {
+
+    void Start() {
+        UpdateText();
+    }
+
+    void Update() {
+        UpdateText();
+    }
+
+    private void UpdateText() {
+        int a = GameManager.GetInstance().GetResources(Resources.ResourcesType.A).count;
+        int b = GameManager.GetInstance().GetResources(Resources.ResourcesType.B).count;
+        int c = GameManager.GetInstance().GetResources(Resources.ResourcesType.C).count;
+        int d = GameManager.GetInstance().GetResources(Resources.ResourcesType.D).count;
+        Text t = GetComponent<Text>();
+        t.text = "A = " + a + " B = " + b + " C = " + c + " D = " + d;
+    }
+}
