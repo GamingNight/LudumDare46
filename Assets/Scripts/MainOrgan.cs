151b0e9204cf2e583908630494226dcde0c72c66
JulesPomme <julian.moreira.fr@gmail.com>
2020-04-19 23:03:14 +0200

    update main organ

---
+++ Assets/Scripts/MainOrgan.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MainOrgan : MonoBehaviour {
+
+    private bool collideWithOtherOrgan;
+    public bool CollideWithOtherOrgan { get { return collideWithOtherOrgan; } }
+    private Animator animator;
+    private bool isSelected;
+    public bool IsSelected { get { return isSelected; } }
+
+    void Start() {
+        collideWithOtherOrgan = false;
+        animator = GetComponent<Animator>();
+        isSelected = false;
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Organ") {
+            collideWithOtherOrgan = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Organ") {
+            collideWithOtherOrgan = false;
+        }
+    }
+
+    void OnMouseEnter() {
+
+        if (animator != null) {
+            animator.SetBool("selected", true);
+        }
+        CursorManager.GetInstance().TriggerSelectionCursor();
+        isSelected = true;
+    }
+
+    void OnMouseExit() {
+
+        if (animator != null) {
+            animator.SetBool("selected", false);
+        }
+        CursorManager.GetInstance().TriggerNavigationCursor();
+        isSelected = false;
+    }
+}
