6002b1f67808a60c5cb0de7cc6c02c56253f8924
JulesPomme <julian.moreira.fr@gmail.com>
2020-04-19 08:57:47 +0200

    Update Organ.cs colors

--- Assets/Scripts/Organ.cs
+++ Assets/Scripts/Organ.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class Organ : MonoBehaviour {
     public Resources.ResourcesType resourcesType;
 
     private Color initColor;
+    private List<Color> initColors;
     private ResourceConf resourceConfCost = new ResourceConf();
     private Resources resourcesReward = new Resources(0, Resources.ResourcesType.A);
     private bool collideWithOtherOrgan;
@@ -24,40 +26,54 @@ public class Organ : MonoBehaviour {
 
     void Start() {
         startHasBeenCalled = true;
-        initColor = GetComponent<SpriteRenderer>().color;
+        if (GetComponent<SpriteRenderer>() != null) {
+            initColor = GetComponent<SpriteRenderer>().color;
+        } else {
+            initColors = new List<Color>();
+            foreach (SpriteRenderer renderer in GetComponentsInChildren<SpriteRenderer>()) {
+                initColors.Add(renderer.color);
+            }
+        }
         collideWithOtherOrgan = false;
 
         // Init Cost and reward Configuration
-        if (resourcesType == Resources.ResourcesType.A)
-        {
+        if (resourcesType == Resources.ResourcesType.A) {
             resourceConfCost.A.count = 1;
             resourcesReward.Set(2, Resources.ResourcesType.A);
-        }
-        else if (resourcesType == Resources.ResourcesType.B)
-        {
+        } else if (resourcesType == Resources.ResourcesType.B) {
             resourceConfCost.A.count = 1;
             resourcesReward.Set(1, Resources.ResourcesType.B);
-        }
-        else if (resourcesType == Resources.ResourcesType.C)
-        {
+        } else if (resourcesType == Resources.ResourcesType.C) {
             resourceConfCost.A.count = 1;
             resourceConfCost.B.count = 1;
             resourcesReward.Set(1, Resources.ResourcesType.C);
-        }
-        else if (resourcesType == Resources.ResourcesType.D)
-        {
+        } else if (resourcesType == Resources.ResourcesType.D) {
             resourceConfCost.A.count = 1;
             resourcesReward.Set(1, Resources.ResourcesType.D);
         }
     }
 
     public void SetToForbiddenColor() {
-        GetComponent<SpriteRenderer>().color = forbiddenColor;
+        if (GetComponent<SpriteRenderer>() != null) {
+            GetComponent<SpriteRenderer>().color = forbiddenColor;
+        } else {
+            foreach (SpriteRenderer renderer in GetComponentsInChildren<SpriteRenderer>()) {
+                renderer.color = forbiddenColor;
+            }
+        }
     }
 
     public void RevertColor() {
         if (startHasBeenCalled) {
-            GetComponent<SpriteRenderer>().color = initColor;
+            if (GetComponent<SpriteRenderer>() != null) {
+                GetComponent<SpriteRenderer>().color = initColor;
+            } else {
+                int i = 0;
+                foreach (SpriteRenderer renderer in GetComponentsInChildren<SpriteRenderer>()) {
+                    renderer.color = initColors[i];
+                    i++;
+                }
+            }
         }
     }
 
@@ -76,17 +92,18 @@ public class Organ : MonoBehaviour {
     public void OnReward() {
         if (resourcesType == Resources.ResourcesType.D) {
             // make the reward only tje first time for resourceD
-            if (rewardDAlreadyDone)
+            if (rewardDAlreadyDone) {
                 return;
+            }
+
             rewardDAlreadyDone = true;
             GameManager.GetInstance().GetResources(resourcesReward.type).Add(resourcesReward.count);
-        }
-        else {
+        } else {
             GameManager.GetInstance().GetResources(resourcesReward.type).Add(resourcesReward.count);
             GameManager.GetInstance().GetResources(resourceConfCost.A.type).Use(resourceConfCost.A.count);
             GameManager.GetInstance().GetResources(resourceConfCost.B.type).Use(resourceConfCost.B.count);
             GameManager.GetInstance().GetResources(resourceConfCost.C.type).Use(resourceConfCost.C.count);
         }
-        
+
     }
 }
