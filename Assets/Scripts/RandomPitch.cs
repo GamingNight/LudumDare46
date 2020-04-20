2ca291bec9301260d0bbcf7847e8b7a37da7d12f
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-04-20 02:20:08 +0200

    des petits sons, bonne nuit

---
+++ Assets/Scripts/RandomPitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPitch : MonoBehaviour
+{
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = Random.Range(1f, 1.1f);
+    }
+
+}
